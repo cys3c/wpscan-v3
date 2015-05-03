2a6082cea0fdaca39aaf65b26c9c2ae1a5cd016d
erwanlr <erwan.lr@gmail.com>
2015-05-03 20:54:29 +0100

    Uses #to_s instead of #message in custom exceptions (wordpress ones were missed)

--- lib/wpscan/errors/wordpress.rb
+++ lib/wpscan/errors/wordpress.rb
@@ -1,14 +1,14 @@
 module WPScan
   # Not WordPress Error
   class NotWordPressError < StandardError
-    def message
+    def to_s
       'The remote website is up, but does not seem to be running WordPress.'
     end
   end
 
   # Invalid Wp Version (used in the WpVersion#new)
   class InvalidWordPressVersion < StandardError
-    def message
+    def to_s
       'The WordPress version is invalid'
     end
   end
