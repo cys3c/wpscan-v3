7544e8ac6a0fe34ba7b496440f8e543b125298e8
erwanlr <erwan.lr@gmail.com>
2015-07-17 19:40:15 +0100

    Uses CMSScanner 0.0.35.1

--- app/models/xml_rpc.rb
+++ app/models/xml_rpc.rb
@@ -4,12 +4,12 @@ module WPScan
     # @return [ Hash ]
     def references
       {
-        url: [
-          'https://www.rapid7.com/db/modules/auxiliary/scanner/http/wordpress_ghost_scanner',
-          'https://www.rapid7.com/db/modules/auxiliary/dos/http/wordpress_xmlrpc_dos',
-          'https://www.rapid7.com/db/modules/auxiliary/scanner/http/wordpress_xmlrpc_login',
-          'https://www.rapid7.com/db/modules/auxiliary/scanner/http/wordpress_pingback_access',
-          'http://codex.wordpress.org/XML-RPC_Pingback_API'
+        url: ['http://codex.wordpress.org/XML-RPC_Pingback_API'],
+        metasploit: [
+          'auxiliary/scanner/http/wordpress_ghost_scanner',
+          'auxiliary/dos/http/wordpress_xmlrpc_dos',
+          'auxiliary/scanner/http/wordpress_xmlrpc_login',
+          'auxiliary/scanner/http/wordpress_pingback_access'
         ]
       }
     end
