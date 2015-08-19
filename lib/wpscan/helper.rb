278e6d93f77d740682c9dfef865769ce06831ae6
erwanlr <erwan.lr@gmail.com>
2015-08-19 17:04:35 +0100

    Removes useless code

--- lib/wpscan/helper.rb
+++ lib/wpscan/helper.rb
@@ -1,10 +1,6 @@
 def read_json_file(file)
-  content = File.open(file).read
-
-  begin
-    JSON.parse(content)
-  rescue => e
-    puts "[ERROR] In JSON file parsing #{file} #{e}"
-    raise
-  end
+  # p "Reading #{file}"
+  JSON.parse(File.read(file))
+rescue => e
+  raise "JSON parsing error in #{file} #{e}"
 end
