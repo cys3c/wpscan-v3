442ab8eba64a7d05ae6ba8af04012237e4cca662
erwanlr <erwan.lr@gmail.com>
2016-11-06 16:37:53 +0000

    Fixes issue due to #zero? methods in CMSScanner

--- app/models/timthumb.rb
+++ app/models/timthumb.rb
@@ -30,8 +30,8 @@ module WPScan
     def vulnerabilities
       vulns = []
 
-      vulns << rce_webshot_vuln if version == false || version > '1.35' && version < '2.8.14' && webshot_enabled?
-      vulns << rce_132_vuln if version == false || version < '1.33'
+      vulns << rce_webshot_vuln if false == version || version > '1.35' && version < '2.8.14' && webshot_enabled?
+      vulns << rce_132_vuln if false == version || version < '1.33'
 
       vulns
     end
